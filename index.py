aabfc05d44ab49ce37a9ab00554ecaad6f14af8e
durgesh <durgesh.singh90@gmail.com>
2022-05-02 09:08:29 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+print ('hello World')
+def add(a,b):
+    return a+b
+def substract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide (a,b):
+    return a//b
+
+print (add(2,4))
+print (substract(2,4))
+print (multiply(2,4))
+print (divide(4,2))
